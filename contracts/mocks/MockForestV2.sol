318eeea5a26570fcf0c4ba364ebdeb8d09b085c1
MASDXI <sirawitt42@gmail.com>
2025-11-12 21:26:03 +0700

    test: add some test to ForestV2

--- contracts/mocks/MockForestV2.sol
+++ contracts/mocks/MockForestV2.sol
@@ -2,14 +2,25 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "../abstracts/ForestTokenV2.sol";
-// import "../policies/FreezeAddress.sol";
-// import "../policies/FreezeBalance.sol";
+import "../policies/FreezeAddress.sol";
+import "../policies/FreezePartialTokens.sol";
+import "../policies/FreezeToken.sol";
 
 // @TODO
-contract MockForestV2 is ForestTokenV2 /**, FreezeAddress, FreezeBalance*/ {
+contract MockForestV2 is ForestTokenV2, FreezeAddress, FreezePartialTokens, FreezeToken {
     constructor(string memory name_, string memory symbol_) ForestTokenV2(name_, symbol_, "") {}
 
     function mint(address account, uint256 value) public {
         _mint(account, value, "");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 value,
+        bytes memory data
+    ) public override checkFrozenAddress(from, to) checkFrozenBalance(from, balanceOf(from, id)) checkFrozenToken(bytes32(id)) {
+        super.safeTransferFrom(from, to, id, value, data);
     }
 }
--- test/contracts/ForestV2/ForestV2.test.js
+++ test/contracts/ForestV2/ForestV2.test.js
@@ -1,10 +1,10 @@
-const {time, loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const {anyValue} = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const {network} = require("hardhat");
 const {expect} = require("chai");
-const {encodeBytes32String, ZeroAddress, solidityPackedKeccak256, getBytes, Interface} = require("ethers");
-const {amount, freezeAmount, transferFrom, transfer, tokenMetadata} = require("../../utils/constant");
-// const {abi} = require("../../../artifacts/contracts/abstracts/ForestTokenV2.sol/ForestTokenV2.json"); 
+const {Interface, zeroPadBytes, toBigInt, encodeBytes32String} = require("ethers");
+const {amount, tokenMetadata} = require("../../utils/constant");
+// const {abi} = require("../../../artifacts/contracts/abstracts/ForestTokenV2.sol/ForestTokenV2.json");
 
 describe("ForestV2", function () {
   async function deployTokenFixture() {
@@ -19,22 +19,58 @@ describe("ForestV2", function () {
     it("Freeze Alice Account and safeTransferFrom", async function () {
       const {token, alice, bob} = await loadFixture(deployTokenFixture);
       const aliceAddress = alice.address;
-      // const bobAddress = bob.address;
+      const bobAddress = bob.address;
       let tx = await token.mint(aliceAddress, amount);
       tx = await tx.wait();
-      // console.log(tx.logs);
-      let abi = [ "event TransactionCreated(bytes32 indexed root, bytes32 id, address indexed from)" ];
+      // Extract token id from event.
+      let abi = ["event TransactionCreated(bytes32 indexed root, bytes32 id, address indexed from)"];
       let interface = new Interface(abi);
-      let log = interface.parseLog(tx.logs[0]); 
-      console.log("🚀 ~ log:", log)
+      let tokenId = interface.parseLog(tx.logs[0]).args[0];
+      await token.setAddressFrozen(aliceAddress, true);
+
+      await expect(
+        token
+          .connect(alice)
+          .safeTransferFrom(aliceAddress, bobAddress, toBigInt(tokenId), amount, encodeBytes32String("")),
+      ).to.be.reverted;
     });
 
     it("Freeze Alice Balance and safeTransferFrom", async function () {
       const {token, alice, bob} = await loadFixture(deployTokenFixture);
+      const aliceAddress = alice.address;
+      const bobAddress = bob.address;
+      let tx = await token.mint(aliceAddress, amount);
+      tx = await tx.wait();
+      // Extract token id from event.
+      let abi = ["event TransactionCreated(bytes32 indexed root, bytes32 id, address indexed from)"];
+      let interface = new Interface(abi);
+      let tokenId = interface.parseLog(tx.logs[0]).args[0];
+      await token.freezePartialTokens(aliceAddress, amount);
+
+      await expect(
+        token
+          .connect(alice)
+          .safeTransferFrom(aliceAddress, bobAddress, toBigInt(tokenId), amount, encodeBytes32String("")),
+      ).to.be.reverted;
     });
 
     it("Freeze Alice Token and safeTransferFrom", async function () {
       const {token, alice, bob} = await loadFixture(deployTokenFixture);
+      const aliceAddress = alice.address;
+      const bobAddress = bob.address;
+      let tx = await token.mint(aliceAddress, amount);
+      tx = await tx.wait();
+      // Extract token id from event.
+      let abi = ["event TransactionCreated(bytes32 indexed root, bytes32 id, address indexed from)"];
+      let interface = new Interface(abi);
+      let tokenId = interface.parseLog(tx.logs[0]).args[0];
+      await token.freezeToken(tokenId);
+
+      await expect(
+        token
+          .connect(alice)
+          .safeTransferFrom(aliceAddress, bobAddress, toBigInt(tokenId), amount, encodeBytes32String("")),
+      ).to.be.reverted;
     });
 
     it("Freeze at root and safeTransferFrom", async function () {
